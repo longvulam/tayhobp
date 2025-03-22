5604fa8c37de7ca9a5758c85e052eeb0a8538926
Vu Lam Long <lamlv.ca@gmail.com>
2025-03-22 11:19:34 -0700

    remove unused var error from eslint rule attempt #2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
+      'next/typescript/no-unused-vars': 'off',
+      '@typescript/no-unused-vars': 'off',
     },
   }),
 ];
