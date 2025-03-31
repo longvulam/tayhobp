a17e4e1fcc58e337beb24b2031df2947ace16942
Vu Lam Long <lamlv.ca@gmail.com>
2025-03-30 22:29:54 -0700

    Remove getImageFiles()

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -3,13 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import { promises as fs } from 'fs'
 import path from 'path'
 
-export const getImageFiles = async () => {
-    const imageDirectory = path.join(process.cwd(), '/public/assets/gallery');
-    const imageFilenames = await fs.readdir(imageDirectory)
-    console.log(imageFilenames)
-
-    return imageFilenames;
-}
 export async function GET(request: NextRequest) {
     const imageDirectory = path.join(process.cwd(), '/public/assets/gallery');
     const imageFilenames = await fs.readdir(imageDirectory)
